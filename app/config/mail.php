50b14da106aacb79222c8a19934f28df6d121377
crynobone <crynobone@gmail.com>
2013-06-09 16:51:33 +0800

    Add mail.pretend based on laravel/laravel@e526c08. Closes #9.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- app/config/mail.php
+++ app/config/mail.php
@@ -107,5 +107,18 @@ return array(
 	*/
 
 	'sendmail' => '/usr/sbin/sendmail -bs',
+	
+	/*
+	|--------------------------------------------------------------------------
+	| Mail "Pretend"
+	|--------------------------------------------------------------------------
+	|
+	| When this option is enabled, e-mail will not actually be sent over the
+	| web and will instead be written to your application's logs files so
+	| you may inspect the message. This is great for local development.
+	|
+	*/
+
+	'pretend' => false,
 
 );
