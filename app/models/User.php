29e4acb57bfd757dfd7e94f530e014246efb9d81
crynobone <crynobone@gmail.com>
2014-06-13 02:27:45 +0800

    Simplify the base User class.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- app/models/User.php
+++ app/models/User.php
@@ -2,20 +2,7 @@
 
 use Orchestra\Model\User as Eloquent;
 
-class User extends Eloquent {
-
-	/**
-	 * The database table used by the model.
-	 *
-	 * @var string
-	 */
-	protected $table = 'users';
-
-	/**
-	 * The attributes excluded from the model's JSON form.
-	 *
-	 * @var array
-	 */
-	protected $hidden = array('password');
-
+class User extends Eloquent
+{
+	use SoftDeletingTrait;
 }
