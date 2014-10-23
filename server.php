00772380bcbf4740fb498effd68d140fd33ea5f3
crynobone <crynobone@gmail.com>
2014-10-24 07:55:54 +0800

    Add temporary fixes to using php artisan serve.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- server.php
+++ server.php
@@ -4,16 +4,15 @@ $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 $uri = urldecode($uri);
 
-$paths = require __DIR__.'/bootstrap/paths.php';
+$path = __DIR__.'/public';
 
-$requested = $paths['public'].$uri;
+$requested = $path.$uri;
 
 // This file allows us to emulate Apache's "mod_rewrite" functionality from the
 // built-in PHP web server. This provides a convenient way to test a Laravel
 // application without having installed a "real" web server software here.
-if ($uri !== '/' && file_exists($requested))
-{
-	return false;
+if ($uri !== '/' && file_exists($requested)) {
+    return false;
 }
 
-require_once $paths['public'].'/index.php';
+require_once $path.'/index.php';
