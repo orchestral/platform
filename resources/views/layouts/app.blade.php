6b8f227821671b1ddf2d0fe7bac9417e2571425e
crynobone <crynobone@gmail.com>
2014-11-26 20:57:49 +0800

    Fixes blade syntax.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,7 +34,7 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="navbar-brand" href="{!! handles('app::/') !!}">{!! memorize('site.name') !}}</a>
+				<a class="navbar-brand" href="{!! handles('app::/') !!}">{!! memorize('site.name') !!}</a>
 			</div>
 
 			<div id="navbar" class="navbar-collapse collapse">
