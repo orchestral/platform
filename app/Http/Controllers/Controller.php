5c92835d8ef5cb10844b825b6ed3eb7d415cdbe6
crynobone <crynobone@gmail.com>
2014-11-26 10:37:44 +0800

    Add controller response trait.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,8 +1,9 @@
 <?php namespace App\Http\Controllers;
 
 use Orchestra\Routing\Controller as BaseController;
+use Orchestra\Support\Traits\ControllerResponseTrait;
 
 abstract class Controller extends BaseController
 {
-    //
+    use ControllerResponseTrait;
 }
