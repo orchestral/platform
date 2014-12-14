c2674d9f1960c3a3b7a6056c0429f48ddb48d0da
crynobone <crynobone@gmail.com>
2014-12-14 13:04:46 +0800

    Simplify the default testcase.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,41 +1,9 @@
 <?php
 
-use Orchestra\Foundation\Application;
+use  Orchestra\Foundation\Testing\ApplicationTestCase;
 
-abstract class TestCase extends Orchestra\Testbench\TestCase
+abstract class TestCase extends ApplicationTestCase
 {
-    /**
-     * Get application timezone.
-     *
-     * @return string
-     */
-    protected function getApplicationTimezone($app)
-    {
-        return $app['config']['app.timezone'];
-    }
-
-     /**
-     * Get application aliases.
-     *
-     * @param  \Illuminate\Foundation\Application  $app
-     * @return array
-     */
-    protected function getApplicationAliases($app)
-    {
-        return $app['config']['app.aliases'];
-    }
-
-    /**
-     * Get application providers.
-     *
-     * @param  \Illuminate\Foundation\Application  $app
-     * @return array
-     */
-    protected function getApplicationProviders($app)
-    {
-        return $app['config']['app.providers'];
-    }
-
     /**
      * Get base path.
      *
@@ -44,41 +12,5 @@ abstract class TestCase extends Orchestra\Testbench\TestCase
     protected function getBasePath()
     {
         return realpath(__DIR__.'/../');
-    }
-
-    /**
-     * Resolve application implementation.
-     *
-     * @return \Illuminate\Foundation\Application
-     */
-    protected function resolveApplication()
-    {
-        $app = new Application($this->getBasePath());
-
-        $app->bind('Illuminate\Foundation\Bootstrap\LoadConfiguration', 'Orchestra\Config\Bootstrap\LoadConfiguration');
-
-        return $app;
-    }
-
-    /**
-     * Resolve application Console Kernel implementation.
-     *
-     * @param  \Illuminate\Foundation\Application  $app
-     * @return void
-     */
-    protected function resolveApplicationConsoleKernel($app)
-    {
-        $app->singleton('Illuminate\Contracts\Console\Kernel', 'App\Console\Kernel');
-    }
-
-    /**
-     * Resolve application HTTP Kernel implementation.
-     *
-     * @param  \Illuminate\Foundation\Application  $app
-     * @return void
-     */
-    protected function resolveApplicationHttpKernel($app)
-    {
-        $app->singleton('Illuminate\Contracts\Http\Kernel', 'App\Http\Kernel');
     }
 }
