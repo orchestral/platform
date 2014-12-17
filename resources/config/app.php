67709877d5904b03b2d5e6f3a286c70df2d3bc97
crynobone <crynobone@gmail.com>
2014-12-17 10:50:15 +0800

    Update to laravel/laravel@8d9bbc1
    Signed-off-by: crynobone <crynobone@gmail.com>

--- resources/config/app.php
+++ resources/config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'debug' => (bool) getenv('APP_DEBUG') ?: false,
+    'debug' => env('APP_DEBUG') ?: false,
 
     /*
     |--------------------------------------------------------------------------
@@ -26,7 +26,7 @@ return [
     |
     */
 
-    'url' => 'http://localhost',
+    'url' => env('APP_URL') ?: 'http://localhost',
 
     /*
     |--------------------------------------------------------------------------
@@ -78,7 +78,7 @@ return [
     |
     */
 
-    'key' => 'YourSecretKey!!!',
+    'key' => env('APP_KEY') ?: 'YourSecretKey!!!',
 
     'cipher' => MCRYPT_RIJNDAEL_128,
 
