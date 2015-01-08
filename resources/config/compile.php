fa345e33dcce75e43734908f92fb65ed5fd557ec
crynobone <crynobone@gmail.com>
2015-01-08 10:30:58 +0800

    Add compile config options.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- resources/config/compile.php
+++ resources/config/compile.php
@@ -26,6 +26,21 @@ return [
 
     /*
     |--------------------------------------------------------------------------
+    | Compiled Config Files
+    |--------------------------------------------------------------------------
+    |
+    | Here you may list config namespace/file which define a "compiles" function
+    | that returns additional files that should be compiled, providing an
+    | easy way to get common files from any packages you are utilizing.
+    |
+    */
+
+    'config' => [
+        //
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
     | Compiled File Providers
     |--------------------------------------------------------------------------
     |
