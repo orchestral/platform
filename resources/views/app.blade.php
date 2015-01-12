a6bc95610acb128d777a5fc65129e89bc9c61d8b
crynobone <crynobone@gmail.com>
2015-01-12 19:24:36 +0800

    Simplify some stuff.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Orchestra Platform</title>
+    @title()
 
     <!-- Bootstrap -->
     <link href="/css/app.css" rel="stylesheet">
@@ -29,7 +29,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#">Laravel</a>
+                <a class="navbar-brand" href="#">{{ memorize('site.name') }}</a>
             </div>
 
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
