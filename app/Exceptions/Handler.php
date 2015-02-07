58b8202b0c94cd207ec4c3091f8fda8b66448019
crynobone <crynobone@gmail.com>
2015-02-07 16:11:33 +0800

    Update to laravel/laravel@35acc54
    Signed-off-by: crynobone <crynobone@gmail.com>

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -36,10 +36,6 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $e)
     {
-        if ($this->isHttpException($e)) {
-            return $this->renderHttpException($e);
-        } else {
-            return parent::render($request, $e);
-        }
+        return parent::render($request, $e);
     }
 }
