9458f1256c48b5473974579f000511fb60f8ea54
crynobone <crynobone@gmail.com>
2015-02-23 10:03:51 +0800

    Convert to tabs and remove inline styling.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -47,11 +47,9 @@
 
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary" style="margin-right: 15px;">
-                                    Login
-                                </button>
+                                <button type="submit" class="btn btn-primary">Login</button>
 
-                                <a href="{{ handles('app::password/email') }}">Forgot Your Password?</a>
+                                <a class="btn btn-link" href="{{ handles('app::password/email') }}">Forgot Your Password?</a>
                             </div>
                         </div>
                     </form>
