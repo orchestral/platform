cfe89d7a9004b92312bda1536094de69a255767f
crynobone <crynobone@gmail.com>
2015-05-05 07:51:23 +0800

    No need to override this by default anymore.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -6,15 +6,11 @@ use Orchestra\Foundation\Http\Middleware\VerifyCsrfToken as BaseVerifier;
 class VerifyCsrfToken extends BaseVerifier
 {
     /**
-     * Handle an incoming request.
+     * The URIs that should be excluded from CSRF verification.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     *
-     * @return mixed
+     * @var array
      */
-    public function handle($request, Closure $next)
-    {
-        return parent::handle($request, $next);
-    }
+    protected $except = [
+        //
+    ];
 }
