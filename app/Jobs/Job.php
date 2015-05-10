668a699a02da474655505f6462e714963ae9ac0d
crynobone <crynobone@gmail.com>
2015-05-10 12:28:16 +0800

    Update to laravel/laravel@d502747
    Signed-off-by: crynobone <crynobone@gmail.com>

--- app/Jobs/Job.php
+++ app/Jobs/Job.php
@@ -2,5 +2,16 @@
 
 abstract class Job
 {
-    //
+    /**
+     * The name of the queue the job should be sent to.
+     *
+     * @var string
+     */
+    public $queue;
+    /**
+     * The seconds before the job should be made available.
+     *
+     * @var int
+     */
+    public $delay;
 }
