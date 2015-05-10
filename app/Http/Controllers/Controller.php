8eaa72c65a62450a022acc73c86e3738d85d07ab
crynobone <crynobone@gmail.com>
2015-05-10 10:40:10 +0800

    Update to laravel/laravel@b21d263
    Signed-off-by: crynobone <crynobone@gmail.com>

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,11 +1,11 @@
 <?php namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Bus\DispatchesCommands;
+use Illuminate\Foundation\Bus\DispatchesJobs;
 use Orchestra\Routing\Controller as BaseController;
 use Orchestra\Routing\Traits\ControllerResponseTrait;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 
 abstract class Controller extends BaseController
 {
-    use ControllerResponseTrait, DispatchesCommands, ValidatesRequests;
+    use ControllerResponseTrait, DispatchesJobs, ValidatesRequests;
 }
