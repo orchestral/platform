fad13ebcf7b1252faeda826f0ff40a6d96c3cf43
crynobone <crynobone@gmail.com>
2015-07-04 16:10:17 +0800

    Update to laravel/laravel@ef95871 (for 3.1.x)
    Signed-off-by: crynobone <crynobone@gmail.com>

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -16,7 +16,6 @@ return [
     'password' => 'Passwords must be at least six characters and match the confirmation.',
     'reset'    => 'Password has been reset!',
     'sent'     => 'Password reminder sent!',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
     'token'    => 'This password reset token is invalid.',
     'user'     => "We can't find a user with that e-mail address.",
 
