e3d2cb4b972317d8e571af6dfe8fe8138878dc0a
crynobone <crynobone@gmail.com>
2015-08-08 21:24:12 +0800

    Update docblock.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -23,7 +23,7 @@ class Handler extends ExceptionHandler
      *
      * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
      *
-     * @param  \Exception  $e
+     * @param  \Throwable  $e
      *
      * @return void
      */
@@ -36,7 +36,7 @@ class Handler extends ExceptionHandler
      * Render an exception into an HTTP response.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $e
+     * @param  \Throwable  $e
      *
      * @return \Illuminate\Http\Response
      */
