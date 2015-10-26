0ce82bdf1e275fcf28eb92157015c100b218cf1f
crynobone <crynobone@gmail.com>
2015-10-27 07:25:15 +0800

    Rename REDIS_DB to REDIS_DATABASE to be consistent with Lumen usage
    Also add REDIS_PASSWORD for usage with RedisCloud.
    
    Signed-off-by: crynobone <crynobone@gmail.com>

--- resources/config/database.php
+++ resources/config/database.php
@@ -118,7 +118,8 @@ return [
         'default' => [
             'host'     => env('REDIS_HOST', '127.0.0.1'),
             'port'     => env('REDIS_PORT', 6379),
-            'database' => env('REDIS_DB', 0),
+            'database' => env('REDIS_DATABASE', 0),
+            'password' => env('REDIS_PASSWORD'),
         ],
 
     ],
