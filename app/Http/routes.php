cdbd4e89fd1007c043265e3b86a42aa076337aab
Taylor Otwell <taylorotwell@gmail.com>
2015-12-10 07:01:02 +0800

    Remove terminology for "web routes".
    Signed-off-by: crynobone <crynobone@gmail.com>

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,15 +14,11 @@
 $router->get('/', 'WelcomeController@index');
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
 | This route group applies the "web" middleware group to every route
 | it contains. The "web" middleware group is defined in your HTTP
 | kernel and includes session state, CSRF protection, and more.
-|
 */
+
 Route::group(['middleware' => 'web'], function () {
     $router->get('home', 'HomeController@index');
 });
