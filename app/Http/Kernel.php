f1ad21ffaa4ac194e9258425762e9a8e2203d5bb
Taylor Otwell <taylorotwell@gmail.com>
2015-12-14 07:13:33 +0800

    Note of explanation.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -7,6 +7,8 @@ class Kernel extends HttpKernel
     /**
      * The application's global HTTP middleware stack.
      *
+     * These middleware are run during every request to your application.
+     *
      * @var array
      */
     protected $middleware = [
@@ -34,6 +36,8 @@ class Kernel extends HttpKernel
 
     /**
      * The application's route middleware.
+     *
+     * These middleware may be assigned to groups or used individually.
      *
      * @var array
      */
