22ef412a4808a49cb3495911c833eb6ae2864481
Taylor Otwell <taylorotwell@gmail.com>
2015-12-17 13:12:57 +0800

    Use provider for consistent language.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- resources/config/auth.php
+++ resources/config/auth.php
@@ -14,7 +14,7 @@ return [
     */
 
     'defaults' => [
-        'guard' => 'web',
+        'guard'     => 'web',
         'passwords' => 'users',
     ],
 
@@ -25,9 +25,9 @@ return [
     |
     | Next, you may define every authentication guard for your application.
     | Of course, a great default configuration has been defined for you
-    | here which uses "session" storage and the Eloquent user source.
+    | here which uses session storage and the Eloquent user provider.
     |
-    | All authentication drivers have a user "source". This defines how the
+    | All authentication drivers have a user provider. This defines how the
     | users are actually retrieved out of your database or other storage
     | mechanisms used by this application to persist your user's data.
     |
@@ -37,22 +37,22 @@ return [
 
     'guards' => [
         'web' => [
-            'driver' => 'session',
-            'source' => 'users',
+            'driver'   => 'session',
+            'provider' => 'users',
         ],
 
         'api' => [
-            'driver' => 'token',
-            'source' => 'users',
+            'driver'   => 'token',
+            'provider' => 'users',
         ],
     ],
 
     /*
     |--------------------------------------------------------------------------
-    | User Sources
+    | User Providers
     |--------------------------------------------------------------------------
     |
-    | All authentication drivers have a user "source". This defines how the
+    | All authentication drivers have a user provider. This defines how the
     | users are actually retrieved out of your database or other storage
     | mechanisms used by this application to persist your user's data.
     |
@@ -64,7 +64,7 @@ return [
     |
     */
 
-    'sources' => [
+    'providers' => [
         'users' => [
             'driver' => 'eloquent',
             'model'  => App\User::class,
@@ -97,10 +97,10 @@ return [
 
     'passwords' => [
         'users' => [
-            'source' => 'users',
-            'email'  => 'emails.auth.password',
-            'table'  => 'password_resets',
-            'expire' => 60,
+            'provider' => 'users',
+            'email'    => 'emails.auth.password',
+            'table'    => 'password_resets',
+            'expire'   => 60,
         ],
     ],
 
