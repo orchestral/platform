27434944f293d69ef5d6ba9419ac4d406fe7b4a2
crynobone <crynobone@gmail.com>
2015-12-27 15:57:48 +0800

    Reorder middleware list.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -50,9 +50,9 @@ class Kernel extends HttpKernel
     protected $routeMiddleware = [
         'auth' => Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
-        'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'backend' => \Orchestra\Foundation\Http\Middleware\UseBackendTheme::class,
         'can' => \Orchestra\Foundation\Http\Middleware\Can::class,
         'guest' => Middleware\RedirectIfAuthenticated::class,
+        'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
     ];
 }
