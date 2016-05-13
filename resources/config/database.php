c1b8cffe90ae29a467d96f139a0f3cf73a4f8808
crynobone <crynobone@gmail.com>
2016-05-13 15:13:33 +0800

    Fixes typo when transfering commit from laravel/laravel.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- resources/config/database.php
+++ resources/config/database.php
@@ -61,7 +61,7 @@ return [
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
-            'strict'    => strict,
+            'strict'    => true,
             'engine'    => null,
         ],
 
