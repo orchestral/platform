4b91b104834aca118572e77ba3c21dbe20960ea8
crynobone <crynobone@gmail.com>
2016-07-12 08:46:29 +0800

    Just use facades in service providers.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -12,9 +12,7 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        'App\Events\SomeEvent' => [
-            'App\Listeners\EventListener',
-        ],
+        // 'App\Events\SomeEvent' => ['App\Listeners\EventListener'],
     ];
 
     /**
