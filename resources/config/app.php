c674ff0f68845f4dbbca27c65ebe3c3a711d9e22
Mior Muhammad Zaki <crynobone@gmail.com>
2018-06-05 11:50:43 +0800

    Readd debug configuration.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- resources/config/app.php
+++ resources/config/app.php
@@ -30,6 +30,19 @@ return [
 
     /*
     |--------------------------------------------------------------------------
+    | Application Debug Mode
+    |--------------------------------------------------------------------------
+    |
+    | When your application is in debug mode, detailed error messages with
+    | stack traces will be shown on every error that occurs within your
+    | application. If disabled, a simple generic error page is shown.
+    |
+    */
+
+    'debug' => env('APP_DEBUG', false),
+
+    /*
+    |--------------------------------------------------------------------------
     | Application URL
     |--------------------------------------------------------------------------
     |
