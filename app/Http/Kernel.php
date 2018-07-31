df0ec3fd02340859866ea74908a013afaf5b5ee8
Mior Muhammad Zaki <crynobone@gmail.com>
2018-08-01 06:50:51 +0800

    Sync Laravel 5.6 skeleton
    Updated to laravel/laravel@3e43e6b1d04793c3837ad383f54aae062c085fd8
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -14,7 +14,7 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
-        \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
+        Middleware\CheckForMaintenanceMode::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
