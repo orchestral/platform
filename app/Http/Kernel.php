2eeeccede562449d0087a325624eb823faf6f61c
Mior Muhammad Zaki <crynobone@gmail.com>
2018-08-03 10:12:40 +0800

    Update Laravel 5.7 skeleton.
    Updated to laravel/laravel@c641edfb02eb6e8c7dac0123aece227e480ff58e
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -67,5 +67,6 @@ class Kernel extends HttpKernel
         'guest' => Middleware\RedirectIfAuthenticated::class,
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
     ];
 }
