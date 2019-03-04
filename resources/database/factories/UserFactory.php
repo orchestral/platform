d28d23ed4d46b697b1b0160656dcf155b92dc999
Mior Muhammad Zaki <crynobone@gmail.com>
2019-03-04 13:57:01 +0800

    Update user factory.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- resources/database/factories/UserFactory.php
+++ resources/database/factories/UserFactory.php
@@ -20,7 +20,8 @@ $factory->define(User::class, function (Faker $faker) {
         'fullname' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'email_verified_at' => now(),
         'remember_token' => Str::random(10),
-        'status' => App\User::VERIFIED,
+        'status' => User::VERIFIED,
     ];
 });
