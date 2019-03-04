7e40f50ff9a779142aaa8e7933ba388bc592ba15
Mior Muhammad Zaki <crynobone@gmail.com>
2019-03-04 09:16:50 +0800

    Update Laravel 5.8 Skeleton
    Updated to https://github.com/laravel/laravel/commit/3001f3c6e232ba7ce2ecdbdfe6e43b4c64ee05ad
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- resources/config/queue.php
+++ resources/config/queue.php
@@ -7,12 +7,9 @@ return [
     | Default Queue Connection Name
     |--------------------------------------------------------------------------
     |
-    | The Laravel queue API supports a variety of back-ends via an unified
+    | Laravel's queue API supports an assortment of back-ends via a single
     | API, giving you convenient access to each back-end using the same
     | syntax for every one. Here you may define a default connection.
-    |
-    | Supported: "null", "sync", "database", "beanstalkd",
-    |            "sqs", "redis"
     |
     */
 
@@ -49,16 +46,16 @@ return [
             'host' => 'localhost',
             'queue' => 'default',
             'retry_after' => 90,
+            'block_for' => 0,
         ],
 
         'sqs' => [
             'driver' => 'sqs',
-            'key' => 'your-public-key',
-            'secret' => 'your-secret-key',
-            'queue' => 'your-queue-url',
-            'prefix' => 'https://sqs.us-east-1.amazonaws.com/your-account-id',
-            'queue' => 'your-queue-name',
-            'region' => 'us-east-1',
+            'key' => env('AWS_ACCESS_KEY_ID'),
+            'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            'prefix' => env('SQS_PREFIX', 'https://sqs.us-east-1.amazonaws.com/your-account-id'),
+            'queue' => env('SQS_QUEUE', 'your-queue-name'),
+            'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
         ],
 
         'redis' => [
--- resources/database/factories/UserFactory.php
+++ resources/database/factories/UserFactory.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\User;
 use Illuminate\Support\Str;
 use Faker\Generator as Faker;
 
@@ -14,13 +15,11 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(App\User::class, function (Faker $faker) {
-    static $password;
-
+$factory->define(User::class, function (Faker $faker) {
     return [
         'fullname' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
+        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
         'status' => App\User::VERIFIED,
     ];
