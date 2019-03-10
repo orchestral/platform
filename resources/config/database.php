898a875e1e11e3ab9527518fbc832135160157f5
Mior Muhammad Zaki <crynobone@gmail.com>
2019-03-10 21:52:16 +0800

    Fixes typo.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- resources/config/database.php
+++ resources/config/database.php
@@ -35,7 +35,7 @@ return [
 
         'testing' => [
             'driver' => 'sqlite',
-            'datebase' => ':memory:',
+            'database' => ':memory:',
         ],
 
         'sqlite' => [
