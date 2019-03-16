03c6d2c73f9d08de6b93e91905db7b21709295d8
Mior Muhammad Zaki <crynobone@gmail.com>
2019-03-16 16:23:26 +0800

    Bump package configuration.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-let mix = require('laravel-mix');
+const mix = require('laravel-mix');
 
 /*
  |--------------------------------------------------------------------------
@@ -12,4 +12,4 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-.sass('resources/sass/app.scss', 'public/css');
+   .sass('resources/sass/app.scss', 'public/css');
