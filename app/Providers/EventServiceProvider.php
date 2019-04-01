e37e410e5b1c4626a24e70270050b48e09c6b813
Mior Muhammad Zaki <crynobone@gmail.com>
2019-04-01 16:07:47 +0800

    Register installation events.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace App\Providers;
 
-use Illuminate\Auth\Events\Registered;
-use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
+use Orchestra\Installation\Events\InstallationCompleted;
+use Orchestra\Installation\Listeners\MigrateDatabaseSchema;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
 class EventServiceProvider extends ServiceProvider
@@ -14,8 +14,8 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        Registered::class => [
-            SendEmailVerificationNotification::class,
+        InstallationCompleted::class => [
+            MigrateDatabaseSchema::class,
         ],
     ];
 
