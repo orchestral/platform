640d3351216e4541226d9c2e3773e05967f16db8
Mior Muhammad Zaki <crynobone@gmail.com>
2019-09-14 16:37:35 +0800

    Update Laravel 6 skeleton
    Updated to
    <https://github.com/laravel/laravel/commit/c70c986e58fe1a14f7c74626e6e97032d4084d5f>
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- config/logging.php
+++ config/logging.php
@@ -1,5 +1,6 @@
 <?php
 
+use Monolog\Handler\NullHandler;
 use Monolog\Handler\StreamHandler;
 use Monolog\Handler\SyslogUdpHandler;
 
@@ -87,6 +88,11 @@ return [
         'errorlog' => [
             'driver' => 'errorlog',
             'level' => 'debug',
+        ],
+
+        'null' => [
+            'driver' => 'monolog',
+            'handler' => NullHandler::class,
         ],
     ],
 ];
