808092a92c9b8b22edd94b5b3d20fc7bd2d06f74
Mior Muhammad Zaki <crynobone@gmail.com>
2019-10-11 09:39:52 +0800

    Add auth.password_timeout config.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- config/auth.php
+++ config/auth.php
@@ -120,4 +120,17 @@ return [
     'registers' => [
         'email' => 'emails.notification',
     ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Confirmation Timeout
+    |--------------------------------------------------------------------------
+    |
+    | Here you may define the amount of seconds before a password confirmation
+    | times out and the user is prompted to re-enter their password via the
+    | confirmation screen. By default, the timeout lasts for three hours.
+    |
+    */
+
+    'password_timeout' => 10800,
 ];
