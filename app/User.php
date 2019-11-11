e516f26c8a680d871b865190d04787afd4e42921
Mior Muhammad Zaki <crynobone@gmail.com>
2019-11-11 10:07:15 +0800

    Add alias.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- app/User.php
+++ app/User.php
@@ -6,5 +6,13 @@ use Orchestra\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    //
+    /**
+     * Get user's name (using alias).
+     *
+     * @return string
+     */
+    public function getNameAttribute(): string
+    {
+        return $this->getAttribute('fullname');
+    }
 }
