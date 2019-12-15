17e7499e946f914cf95a72fe3b1d6d87b3017717
Mior Muhammad Zaki <crynobone@gmail.com>
2019-12-15 20:55:46 +0800

    Update logging and session config.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- config/logging.php
+++ config/logging.php
@@ -36,7 +36,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['daily'],
+            'channels' => ['single'],
             'ignore_exceptions' => false,
         ],
 
