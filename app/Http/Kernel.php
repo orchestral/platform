5cc7d1c0c24c7c4ba38548f12b5e936be00bebda
Mior Muhammad Zaki <crynobone@gmail.com>
2020-08-29 17:15:56 +0800

    Update skeleton.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
+        // \App\Http\Middleware\TrustHosts::class,
         Middleware\TrustProxies::class,
         \Fruitcake\Cors\HandleCors::class,
         Middleware\CheckForMaintenanceMode::class,
